5756345113165bbffbff629e9e5d4e56da7a4277
Bryan Hughes <bryanhug@umich.edu>
2019-01-16 21:54:56 -0800

    got it running with new file

--- kml/read.py
+++ kml/read.py
@@ -2,17 +2,27 @@
 from fastkml import kml
 import re
 import os, sys
+import traceback
 
 def parse_placemark(geos, p):
     """Splits the kml description into name, area, lat, lng"""
     # Splits the Placemark description into 25 parts
-    split = re.split(r'<br>', str(p.description))
+    split = re.split(r'<br><br>|<br>', str(p.description))
     
-    #split[3]=area, split[4]=lat, split[5]=lng
-    name = p.name.rstrip().lstrip().replace('/','').replace(',','')
-    area = split[3].split()[1]
-    lat = split[4].split()[1]
-    lng = split[5].split()[1]
+    try:
+        #split[3]=area, split[4]=lat, split[5]=lng
+        name = p.name.rstrip().lstrip().replace('/','').replace(',','')
+        area = split[3].split()[1]
+        lat = split[4].split()[1]
+        lng = split[5].split()[1]
+    except(IndexError) as er:
+        # print (p.description)
+        # print(p(name='area_sqm'))
+        print(name)
+        print(split[4])
+        print(er)
+        traceback.print_exc()
+        sys.exit(0)
     geos.write(name + ',' + area + ',' + lat + ',' + lng + '\n')
 
 with open('FTEAssignments.kml', 'rb') as f:
